2db0687ab114fbf38d4f613c1e04446c569edd5e
AdarshaAP <113161381+AdarshaAP@users.noreply.github.com>
2026-03-28 08:36:28 +0000

    feat: implement dbt silver and gold layers for ICD-10 data
    - Initialize dbt_project in src/dbt_project
    - Create format_icd10_code macro for the ICD-10 decimal rule
    - Implement Silver layer: silver_icd10_ontology to clean and map Bronze data
    - Implement Gold layers: gold_icd10_clinical_index and gold_icd10_billable_leaves
    - Add schema.yml with unique and not_null data tests

--- src/dbt_project/models/gold/gold_icd10_billable_leaves.sql
+++ src/dbt_project/models/gold/gold_icd10_billable_leaves.sql
@@ -1,11 +1,17 @@
 -- Copyright (c) 2026 CoReason, Inc.
 -- Licensed under the Prosperity Public License 3.0
 
-{{ config(materialized='view') }}
+{{ config(
+    materialized='view'
+) }}
 
 SELECT
+    coreason_id,
+    fiscal_year,
     formatted_icd10_code,
+    raw_code_string,
+    short_description,
     long_description,
-    fiscal_year
+    is_billable
 FROM {{ ref('icd10_ontology') }}
 WHERE is_billable = TRUE
